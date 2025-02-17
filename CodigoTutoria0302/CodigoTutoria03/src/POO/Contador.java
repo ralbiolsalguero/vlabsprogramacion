f01bf364ff6321d37576d5da7cc188f3ad7cd716
ralbiolsalguero <raul.albiol@thepower.education>
2025-02-17 23:55:38 +0100

    Códigos

---
+++ CodigoTutoria0302/CodigoTutoria03/src/POO/Contador.java
@@ -0,0 +1,51 @@
+package POO;
+
+public class Contador {
+
+    private int cont;
+
+    //constructor por defecto
+    public Contador() {
+    }
+
+    //constructor con parámetros
+    public Contador(int cont) {
+        if (cont < 0) {
+            this.cont = 0;
+        } else {
+            this.cont = cont;
+        }
+    }
+
+    //constructor copia
+    public Contador(final Contador c) {
+        cont = c.cont;
+    }
+
+    //getter
+    public int getCont() {
+        return cont;
+    }
+
+    //setter
+    public void setCont(int cont) {
+        if (cont < 0) {
+            this.cont = 0;
+        } else {
+            this.cont = cont;
+        }
+    }
+
+    //método incrementar contador
+    public void incrementar() {
+        cont++;
+    }
+
+    //método decrementar contador
+    public void decrementar() {
+        cont--;
+        if (cont < 0) {
+            cont = 0;
+        }
+    }
+}
